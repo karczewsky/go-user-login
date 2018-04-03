d9cf1f1d2a891e96e3b4b0bd5d41dd8c0f6ed7c5
wolpear <wolpear@users.noreply.github.com>
2018-04-03 16:41:41 +0200

    Basic login form

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -10,7 +13,31 @@ func main() {
 	r := mux.NewRouter()
 
 	r.Handle("/", http.FileServer(http.Dir("./views/")))
+	r.HandleFunc("/internal", internalPageHandler)
+	// Obsluga statycznych plikow
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	r.HandleFunc("/login", loginHandler)
 
-	http.ListenAndServe(":8080", r)
+	// Ustawienie middleware - logger
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	http.ListenAndServe(":8080", loggedRouter)
+}
+
+func internalPageHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "XD LOL")
+}
+
+func loginHandler(response http.ResponseWriter, request *http.Request) {
+	login := request.FormValue("username")
+	pass := request.FormValue("password")
+
+	redirectURL := "/"
+	if login != "" && pass != "" {
+		// Walidacja danych
+
+		if login == "admin" && pass == "admin" {
+			redirectURL = "/internal"
+		}
+	}
+	http.Redirect(response, request, redirectURL, 302)
 }

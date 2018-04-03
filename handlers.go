edb0668207b1e63b9de18fa4c1bf6ea223f293ee
wolpear <wolpear@users.noreply.github.com>
2018-04-03 19:40:59 +0200

    Restructurization, cookie/session support

---
+++ handlers.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/securecookie"
+)
+
+const internalPage = `
+<h1>Internal</h1>
+<hr>
+<small>User: %s</small>
+<form method="post" action="/logout">
+    <button type="submit">Logout</button>
+</form>
+`
+
+func internalPageHandler(response http.ResponseWriter, request *http.Request) {
+	userName := getUserName(request)
+	if userName != "" {
+		fmt.Fprintf(response, internalPage, userName)
+	} else {
+		http.Redirect(response, request, "/", 302)
+	}
+}
+
+func loginHandler(response http.ResponseWriter, request *http.Request) {
+	login := request.FormValue("username")
+	pass := request.FormValue("password")
+
+	redirectURL := "/"
+	if login != "" && pass != "" {
+		// Walidacja danych
+
+		if login == "admin" && pass == "admin" {
+			setSession(login, response)
+			redirectURL = "/internal"
+		}
+	}
+	http.Redirect(response, request, redirectURL, 302)
+}
+
+func logoutHandler(response http.ResponseWriter, request *http.Request) {
+	clearSession(response)
+	http.Redirect(response, request, "/", 302)
+}
+
+// Session, Cookie related functions
+var cookieHandler = securecookie.New(
+	securecookie.GenerateRandomKey(64),
+	securecookie.GenerateRandomKey(32))
+
+func setSession(userName string, response http.ResponseWriter) {
+	value := map[string]string{
+		"username": userName,
+	}
+
+	if encoded, err := cookieHandler.Encode("session", value); err == nil {
+		cookie := http.Cookie{
+			Name:     "session",
+			Value:    encoded,
+			Path:     "/",
+			HttpOnly: true,
+		}
+		http.SetCookie(response, &cookie)
+		fmt.Printf("Creating cookie -> user: %s", userName)
+	}
+}
+
+func clearSession(response http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
+	}
+	http.SetCookie(response, cookie)
+}
+
+func getUserName(request *http.Request) (userName string) {
+	if cookie, err := request.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+			userName = cookieValue["username"]
+		}
+	}
+	fmt.Printf("LOL %s", userName)
+	return userName
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
@@ -12,32 +11,15 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.Handle("/", http.FileServer(http.Dir("./views/")))
 	r.HandleFunc("/internal", internalPageHandler)
 	// Obsluga statycznych plikow
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	r.Handle("/", http.FileServer(http.Dir("./views/")))
 	r.HandleFunc("/login", loginHandler)
+	r.HandleFunc("/logout", logoutHandler)
 
 	// Ustawienie middleware - logger
 	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 	http.ListenAndServe(":8080", loggedRouter)
-}
-
-func internalPageHandler(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "XD LOL")
-}
-
-func loginHandler(response http.ResponseWriter, request *http.Request) {
-	login := request.FormValue("username")
-	pass := request.FormValue("password")
-
-	redirectURL := "/"
-	if login != "" && pass != "" {
-		// Walidacja danych
-
-		if login == "admin" && pass == "admin" {
-			redirectURL = "/internal"
-		}
-	}
-	http.Redirect(response, request, redirectURL, 302)
 }
